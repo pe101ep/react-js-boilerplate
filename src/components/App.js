8ae823db92920b5f88de048305cd3374acdc7735
Paolo E <paohg10@hotmail.com>
2018-07-20 15:35:36 -0500

    starting react-js-boilerplate project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>React App</h1>
+        <p>Hello React JS</p>
+      </div>
+    );
+  }
+}
+
+export default App;
